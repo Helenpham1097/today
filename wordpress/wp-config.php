5e37eb94725e99d7f823bd0768ce33d12071c417
Helen Pham <helen.pham@sitehost.co.nz>
2025-05-13 12:18:55 +1200

    fix NATS manager test

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress12db' );
+define( 'DB_NAME', 'trangphamdb2' );
 
 /** Database username */
-define( 'DB_USER', 'wordpress12db' );
+define( 'DB_USER', 'trangphamdb2' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Wordpress12db' );
+define( 'DB_PASSWORD', 'Trangphamdb2' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
