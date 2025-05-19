f3e24083b109276b28675ab4398b9a1e0fa9d03d
Helen Pham <helen.pham@sitehost.co.nz>
2025-05-19 16:25:11 +1200

    wordpress13

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'trangphamdb2' );
+define( 'DB_NAME', 'wordpress13db' );
 
 /** Database username */
-define( 'DB_USER', 'trangphamdb2' );
+define( 'DB_USER', 'wordpress13db' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Trangphamdb2' );
+define( 'DB_PASSWORD', 'Wordpress13db' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
