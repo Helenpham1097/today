774d38c9dc1343f00e6d612871eac653151fb63d
Helen Pham <helen.pham@sitehost.co.nz>
2025-08-04 15:05:52 +1200

    Revert "Fix: nat monitoring script"
    This reverts commit f8b7b80628c8e9593a1203e2f9b2ebd23fad4e5d.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'proxydb1' );
+define( 'DB_NAME', 'coffeedb' );
 
 /** Database username */
-define( 'DB_USER', 'proxydb1' );
+define( 'DB_USER', 'coffeedbuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Proxydb1' );
+define( 'DB_PASSWORD', 'Coffeedbuser123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
