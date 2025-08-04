1ce3855b3d23f3a4ec05e8fbed29be9ed02ccaaf
Helen Pham <helen.pham@sitehost.co.nz>
2025-08-04 15:05:54 +1200

    Revert "address comments"
    This reverts commit 3373e66bf856f4f33f9c6eb3e947627367abefc3.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'newversion' );
+define( 'DB_NAME', 'gitwebsite2db' );
 
 /** Database username */
-define( 'DB_USER', 'newversionuser' );
+define( 'DB_USER', 'gitwebsite2dbuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Newversionuser123' );
+define( 'DB_PASSWORD', 'Gitwebsite2dbuser' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
