0465de978ce8b2800ee0621ebd50aaf8085752d9
Helen Pham <helen.pham@sitehost.co.nz>
2025-08-04 15:06:07 +1200

    Revert "for mynewdatabasewebsite"
    This reverts commit 86eb35f1e16a6109b0f65f063fad3f815a509370.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mynewdatabasewebsite' );
+define( 'DB_NAME', 'uswest2db' );
 
 /** Database username */
-define( 'DB_USER', 'mynewdatabasewebsite' );
+define( 'DB_USER', 'helen' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Mynewdatabasewebsite123' );
+define( 'DB_PASSWORD', 'Helen123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
