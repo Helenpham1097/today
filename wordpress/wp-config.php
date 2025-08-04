4fb71bec537e5a287482f492fc4bc8f729cdf856
Helen Pham <helen.pham@sitehost.co.nz>
2025-08-04 15:06:07 +1200

    Revert "for uswest2 website"
    This reverts commit 70f148b2fdd88ae9740429d1b5ad05d97b1f13b3.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'uswest2db' );
+define( 'DB_NAME', 'apsoutheast2db' );
 
 /** Database username */
-define( 'DB_USER', 'helen' );
+define( 'DB_USER', 'apsoutheast2dbuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Helen123' );
+define( 'DB_PASSWORD', 'Apsoutheast2dbuser' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
