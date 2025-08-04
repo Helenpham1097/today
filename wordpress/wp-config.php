0ed0ab406d619557e6c46fde72514c6eee2e138f
Helen Pham <helen.pham@sitehost.co.nz>
2025-08-04 15:06:06 +1200

    Revert "test team three"
    This reverts commit e96f27295cb2ad820864de4ee090976771c20c50.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'temporaldb' );
+define( 'DB_NAME', 'mynewdatabasewebsite' );
 
 /** Database username */
-define( 'DB_USER', 'temporaldbuser' );
+define( 'DB_USER', 'mynewdatabasewebsite' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Temporaldbuser123' );
+define( 'DB_PASSWORD', 'Mynewdatabasewebsite123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
