82d7d47f36c4bb1b419d46ec6287079cfa1355ba
Helen Pham <helen.pham@sitehost.co.nz>
2025-09-09 11:28:23 +1200

    Fix: address comments

--- wordpress10/wp-config.php
+++ wordpress10/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'helenphamwp' );
+define( 'DB_NAME', 'ngoanxinhiu' );
 
 /** Database username */
-define( 'DB_USER', 'helenphamwp' );
+define( 'DB_USER', 'ngoanxinhiu' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Helenphamwp123' );
+define( 'DB_PASSWORD', 'Ngoanxinhiu123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
