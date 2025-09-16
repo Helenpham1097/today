c0dd1eefb07c859d19c622802b7682d845ebd8c7
Helen Pham <helen.pham@sitehost.co.nz>
2025-09-16 15:26:26 +1200

    release & build

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Build for webslice.com
+echo "Starting Build Script"
+
+# Print command output for each command and exit immediately if any command fails (https://man7.org/linux/man-pages/man1/set.1p.html)
+set -ex
+
+COMPOSER_PROCESS_TIMEOUT=2000 composer install --no-interaction -vvv --no-dev --prefer-dist --optimize-autoloader
+npm ci --cache .npm --prefer-offline
+#npm run build:prod
+#php artisan key:generate
+
+echo "Build Script Finished Successfully"
---
+++ scripts/release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Deploy Script for Webslice deploy
+
+# exit immediately if any command fails
+set -e
+
+echo "Clearing Config/Cache & Warming"
