8b72ffbc9023bcb6cd40c4a0300698f2322c6449
Helen Pham <helen.pham@sitehost.co.nz>
2026-02-01 22:04:28 +1300

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,13 +5,16 @@ set -e
 
 echo "Starting WordPress build..."
 
+sleep 2
 echo "Linting code..."
 
+sleep 2
 echo "Building CSS..."
 
+sleep 2
 echo "Building JavaScript..."
 
-
+sleep 2
 echo "Cleaning up..."
 
 echo "Build completed successfully!"
