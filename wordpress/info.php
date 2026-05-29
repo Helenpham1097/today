4f06e905cee0ad8072f8bcf3d25f282d2da703a5
Helen Pham <helen.pham@sitehost.co.nz>
2026-05-29 14:34:39 +1200

    test

--- wordpress/info.php
+++
@@ -1,5 +0,0 @@
-<?php
-$src = '/home/helen-pham/Downloads/webslice/ws_testing/today/wordpress/info.php';
-$dst = '/home/helen-pham/Downloads/webslice/ws_testing/today/wordpress/info2.php';
-
-var_dump(copy($src, $dst));
